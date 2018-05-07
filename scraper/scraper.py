0b13541fae2f7a4409735fa980ce2de28a38e72d
tv <tv@whitespace-solutions.com>
2018-05-07 15:54:37 -0400

    Adding config to scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,8 +6,11 @@ from libs.nprscraper import NPRScraperFunctions
 from libs.sqlcreator import create_alchemy_engine
 
 import sys
+import json
+import pickle
 import os.path
 import argparse
+
 
 from sqlalchemy.orm import sessionmaker
 
@@ -20,7 +23,7 @@ def run_scraper(config, session, url_queue, urls_traversed):
 
     count = session.query(Article).count()
 
-    while len(url_queue):
+    while len(url_queue) > 0:
 
         print("DB count:", count, "Traversed:", len(urls_traversed), "Queue:", len(url_queue))
 
@@ -53,6 +56,15 @@ def run_scraper(config, session, url_queue, urls_traversed):
         session.commit()
         count = session.query(Article).count()
 
+    return url_queue, urls_traversed
+
+def read_pickle(filename):
+    with open(filename, 'rb') as file_hdl:
+        return pickle.load(file_hdl)
+
+def write_pickle(filename, object):
+    with open(filename, 'wb') as file_hdl:
+        pickle.dump(object, file_hdl)
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Scrape NPR website for news')
@@ -60,14 +72,14 @@ def main(argv):
 
     args = parser.parse_args()
 
-    config = args.config
+    config_file = args.config
 
-    if config is None:
+    if config_file is None:
         parser.print_help()
         return
 
-    if not os.path.isfile(config):
-        print("File", config, "not found")
+    if not os.path.isfile(config_file):
+        print("File", config_file, "not found")
         parser.print_help()
         return
 
@@ -76,22 +88,41 @@ def main(argv):
     session = sessionmaker(bind=engine)()
 
     # Read config file
-    # Initialize traversed and to traverse sets based on input
+    with open(config_file) as file_hdl:
+        config = json.load(file_hdl)
 
+    # Initialize traversed and to traverse sets based on input
     start_url = "https://www.npr.org/"
 
-    urls_traversed = set()
+    if "urls_traversed" in config and os.path.isfile(config["urls_traversed"]):
+        urls_traversed = read_pickle(config["urls_traversed"])
+    else:
+        urls_traversed = set()
     urls_traversed.update([r.url for r in session.query(Article.url).distinct()])
 
-    url_queue = set()
+    if "url_queue" in config and os.path.isfile(config["url_queue"]):
+        url_queue = read_pickle(config["url_queue"])
+        print("URL queue:", len(url_queue))
+    else:
+        url_queue = set()
     url_queue.add(start_url)
+
     if start_url in urls_traversed:
         urls_traversed.remove(start_url)
 
+    # ensure any urls we may have traversed are not in there
+    url_queue = url_queue - urls_traversed
+
+    print("Starting run with a queue of", len(url_queue))
     # Perform the run
-    run_scraper(config, session, url_queue, urls_traversed)
+    url_queue, urls_traversed = run_scraper(config, session, url_queue, urls_traversed)
 
     # Depending on configs, save the remaining list
+    if "save_status" in config and config["save_status"]:
+        print("Writing url queue with length", len(url_queue))
+        write_pickle("url_queue.pkl", url_queue)
+        write_pickle("urls_traversed.pkl", urls_traversed)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
